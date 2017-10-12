d585de0c911aa22f02f127e3ed4a16c0fb4eea11
jhon0701 <cjhon0701@gmail.com>
2017-10-12 03:55:01 -0400

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList} from "angularfire2/database"; 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseListObservable } from "angularfire2/database-deprecated"; 
+
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './user.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  username: AngularFireList<{}>;
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.username = af.list('/users');
+
+    this.user = this.afAuth.authState;
+    
+  };
+  usu : Usuario ={
+    name : "",
+    lastname : "",
+    username : "",
+    email : "",
+    password : ""
+  };
+  Send() {
+    this.username.push(this.usu);
+  }
+}
+export class Usuario{
+  name : string;
+  lastname : string;
+  username : string;
+  email : string;
+  password : string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxKJbC2LTLrCUZZOyp3Hl9S53Wk608BBI",
+  authDomain: "proyecto-sistemas-2.firebaseapp.com",
+  databaseURL: "https://proyecto-sistemas-2.firebaseio.com",
+  projectId: "proyecto-sistemas-2",
+  storageBucket: "proyecto-sistemas-2.appspot.com",
+  messagingSenderId: "282454954622"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
